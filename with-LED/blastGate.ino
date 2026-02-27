a2dd23f4b6e19c178bf4b41abf3917aa5dbdbb2c
goombad <168694775+goombad@users.noreply.github.com>
2026-02-27 17:56:15 -0500

    Moved LED pins and added state machine
    Moved LED pins to 10,11,12
    Added state machine structure, we should be thinking about how to use state machine for our actuator
    Enabled pin 2 interrupt as a estop push button, we can change this however we want

---
+++ with-LED/blastGate.ino
@@ -0,0 +1,91 @@
+// C++ code
+
+//motor position enum states
+// we should be switching to state machines as we progress
+// more robust code that will prevent future spaghetti code
+enum motorPos {START, POS25, POS50, POS75, POS100} state, prevState;
+volatile bool isNewState; //checks if state changes
+volatile bool estopPressed = false;  //estop state
+
+void setup()
+{
+  Serial.begin(115200); //baud rate of ESP32
+  pinMode(2, INPUT_PULLUP); //moved LEDS to other pins, 2 and 3 have hardware interrupt which we need
+  attachInterrupt(digitalPinToInterrupt(2), estopISR, FALLING); //enables hardware interrupt on 2, falling edge, triggers estop ISR
+  pinMode(10, OUTPUT); //LED Backward RED //** Probably have to 
+  pinMode(11, OUTPUT); //LED Forward YELLOW
+  pinMode(12, OUTPUT); //LED Idle GREEN
+  pinMode(5, OUTPUT); //negative motor term
+  pinMode(6, OUTPUT); //positive motor term
+  pinMode(9, OUTPUT); //pwm pin
+  pinMode(A0, INPUT); //potentiometer reading pin
+
+  state = START; //sets initial state at origin
+  prevState = POS100; //arbitrary prevState
+}
+
+int STOPPED_DEADBAND = 30;
+
+void estopISR() //handler for estop interrupt
+{
+  estopPressed = true; //triggers interrupt flag
+}
+
+void loop()
+{
+  int potValue = analogRead(A0); //potentiometer reading
+  int speed = map(potValue, 0, 1023, -255, 255); 
+  //maps analog to digital range 0-1023 to -255 to 255
+  //speed deadband
+  if (speed > -STOPPED_DEADBAND && speed < STOPPED_DEADBAND) {
+    speed = 0;
+  } 
+  if (estopPressed)
+  {
+    state = START; //retracts actuator back to start position
+    estopPressed = false; //release estop
+  }
+  switch (state)
+  {
+    case START:
+      break;
+    case POS25: 
+  		break;
+  	case POS50:
+  		break;
+  	case POS75:
+  		break;
+  	case POS100:
+  		break;
+    default: state = START;
+  }
+  prevState = state; //changes state to previous
+
+  Serial.println(speed);//debug
+  if (speed > 0)
+  {
+    digitalWrite(4, LOW); //Idle LED deactivates
+    digitalWrite(5, LOW); //extend actuator motor forward
+    digitalWrite(2, LOW); //Backward LED deactivates
+    digitalWrite(6, HIGH);
+    digitalWrite(3, HIGH); //Forward LED activates
+  }
+  else if (speed < 0)
+  {
+    digitalWrite(4, LOW);  //Idle LED deactivates
+    digitalWrite(5, HIGH); //retract actuator motorbackward
+    digitalWrite(3, LOW);  //Forward LED deactivates
+    digitalWrite(6, LOW);
+    digitalWrite(2, HIGH); //Backward LED activates
+  }
+  else if(speed == 0)
+  {
+    digitalWrite(2, LOW); //Backward LED deactivates
+    digitalWrite(3, LOW); //Forward LED deactivates
+    digitalWrite(4, HIGH); //Idle LED activates
+
+  }
+  analogWrite(9, abs(speed));
+  //absolute so that analog always writes positive
+  
+}
