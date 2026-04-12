595c1a27b1bb6d924c4276523e68c686ef08ba65
AnotherNeko <my_email@example.com>
2026-04-12 10:07:41 -0400

    handle serial during every iteration not just IDLE.

--- blastGate/blastGate.ino
+++ blastGate/blastGate.ino
@@ -98,112 +98,100 @@ void setup()
   pinMode(ENA, OUTPUT); //pwm pin
   pinMode(WIPER, INPUT); //potentiometer reading pin
 
-  { //wiper calibration block
-      WiperCalibration cal;
-      EEPROM.get(wiper_calibration_addr, cal);
-      if(cal.magic == wiper_calibration_magic) {
-          wiperMin = cal.minVal;
-          wiperMax = cal.maxVal;
-      } else {
-          //TODO: get the user's attention that calibration is needed. Even if serial is not connected.
-          // LEDs? Morse code C on the builtin LED?
-          Serial.println("Please jog the gate to closed position");
-          //wait/poll for closing to finish
-          {
-              bool jog_close_was_pressed = false;
-              while (!jog_close_was_pressed || !digitalRead(JOG_CLOSE))
-              {
-                  if (!digitalRead(JOG_CLOSE))
-                  {
-                      motorRetract(minSpeed);
-                      jog_close_was_pressed = true;
-                  } else if (!digitalRead(JOG_OPEN))
-                  {
-                      motorExtend(minSpeed);
-                  } else {
-                      motorStop();
-                  }
-              }
+  {  //wiper calibration block
+    WiperCalibration cal;
+    EEPROM.get(wiper_calibration_addr, cal);
+    if (cal.magic == wiper_calibration_magic) {
+      wiperMin = cal.minVal;
+      wiperMax = cal.maxVal;
+    } else {
+      //TODO: get the user's attention that calibration is needed. Even if serial is not connected.
+      // LEDs? Morse code C on the builtin LED?
+      Serial.println("Please jog the gate to closed position");
+      //wait/poll for closing to finish
+      {
+        bool jog_close_was_pressed = false;
+        while (!jog_close_was_pressed || !digitalRead(JOG_CLOSE)) {
+          if (!digitalRead(JOG_CLOSE)) {
+            motorRetract(minSpeed);
+            jog_close_was_pressed = true;
+          } else if (!digitalRead(JOG_OPEN)) {
+            motorExtend(minSpeed);
+          } else {
+            motorStop();
           }
-          wiperMin = analogRead(WIPER);
-          Serial.println("Please jog the gate to open position");
-          //wait/poll for opening to finish
-            {
-                bool jog_open_was_pressed = false;
-                while (!jog_open_was_pressed || !digitalRead(JOG_OPEN))
-                {
-                    if (!digitalRead(JOG_OPEN))
-                    {
-                        motorExtend(minSpeed);
-                        jog_open_was_pressed = true;
-                    } else if (!digitalRead(JOG_CLOSE))
-                    {
-                        motorRetract(minSpeed);
-                    } else {
-                        motorStop();
-                    }
-                }
-            }
-          wiperMax = analogRead(WIPER);
-
-          //save calibration
-          cal.magic = wiper_calibration_magic;
-          cal.minVal = wiperMin;
-          cal.maxVal = wiperMax;
-          EEPROM.put(wiper_calibration_addr, cal);
+        }
       }
+      wiperMin = analogRead(WIPER);
+      Serial.println("Please jog the gate to open position");
+      //wait/poll for opening to finish
+      {
+        bool jog_open_was_pressed = false;
+        while (!jog_open_was_pressed || !digitalRead(JOG_OPEN)) {
+          if (!digitalRead(JOG_OPEN)) {
+            motorExtend(minSpeed);
+            jog_open_was_pressed = true;
+          } else if (!digitalRead(JOG_CLOSE)) {
+            motorRetract(minSpeed);
+          } else {
+            motorStop();
+          }
+        }
+      }
+      wiperMax = analogRead(WIPER);
+
+      //save calibration
+      cal.magic = wiper_calibration_magic;
+      cal.minVal = wiperMin;
+      cal.maxVal = wiperMax;
+      EEPROM.put(wiper_calibration_addr, cal);
+    }
   }
 
-  state = IDLE; //sets initial state at origin
-  prevState = ESTOP; //arbitrary prevState
+  state = IDLE;       //sets initial state at origin
+  prevState = ESTOP;  //arbitrary prevState
 }
 
-void loop()
-{
-  if (estopPressed)
-  {
+void loop() {
+  if (estopPressed) {
     state = ESTOP; //retracts actuator back to start position
     estopPressed = false; //release estop
   }
-  if (acsAccessChanged)
-  {
-      acsAccessChanged = false;
-      if(digitalRead(ACS_ACCESS))
+  if (acsAccessChanged) {
+    acsAccessChanged = false;
+    if (digitalRead(ACS_ACCESS)) {
+      targetPercent = openSetpoint;
+    } else {
+      targetPercent = 0;
+    }
+    if (state == IDLE) state = MOVING;
+  }
+  if (Serial.available()) {
+    if (Serial.peek() == int('?')) {  //READ case
+      while (Serial.available()) { Serial.read(); }
+      Serial.println(analogRead(WIPER));
+    } else { //WRITE setpoint case
+      targetPercent = Serial.parseInt();  //reads integers only, but the /n remains
+      //TODO: Vincent — replace this with ACS communication protocol
+      while (Serial.available())  //clears /n by reading the serial again
       {
-          targetPercent = openSetpoint;
-      } else {
-          targetPercent = 0;
+        Serial.read();  //reads the serial which is usually /n and clears it
+                        //** somethign to note: if user puts a float, it will run the integer half and the decimal half
       }
-      state = MOVING;
+      targetPercent = constrain(targetPercent, 0, 100);            //clamps targetPercent to a range
+      targetPos = map(targetPercent, 0, 100, wiperMin, wiperMax);  //maps the percent given to an analog reading 0-1023
+      Serial.print("Percent set:");
+      Serial.println(targetPercent);  //print target percent
+      openSetpoint = targetPercent;   //use serial to set the opening amount
+      Serial.print("Position set:");
+      Serial.println(targetPos);  //print target pos
+      if (state == IDLE) state = MOVING; //goes to moving section
+    }
   }
   isNewState = (state != prevState);
-  switch (state)
-  {
-    case IDLE: //TODO: blast gate should respond to query while moving as well as idle.
+  switch (state) {
+    case IDLE:
       if (isNewState) Serial.println("Enter positon from 0-100%");
-      if (Serial.available())
-      {
-        if(Serial.peek() == int('?')) { //READ case
-            while (Serial.available()) { Serial.read(); }
-            Serial.println(analogRead(WIPER));
-            break;
-        } // else: WRITE setpoint case
-        targetPercent = Serial.parseInt(); //reads integers only, but the /n remains
-        //TODO: Vincent — replace this with ACS communication protocol
-        while (Serial.available()) //clears /n by reading the serial again
-        {
-          Serial.read(); //reads the serial which is usually /n and clears it
-          //** somethign to note: if user puts a float, it will run the integer half and the decimal half
-        }
-        targetPercent = constrain(targetPercent, 0, 100); //clamps targetPercent to a range
-        targetPos = map(targetPercent, 0, 100,  wiperMin, wiperMax); //maps the percent given to an analog reading 0-1023
-        Serial.print("Percent set:");
-        Serial.println(targetPercent); //print target percent
-        openSetpoint = targetPercent; //use serial to set the opening amount
-        Serial.print("Position set:");
-        Serial.println(targetPos); //print target pos
-        state = MOVING; //goes to moving section
-      }
       break;
     case MOVING:
       targetPos = map(targetPercent, 0, 100, wiperMin, wiperMax);
@@ -234,5 +222,4 @@ void loop()
 
   currentPos = analogRead(WIPER); //position reading, analog 0 - 1023
   currentPercent = map(currentPos, wiperMin, wiperMax, 0, 100); //percent reading, changes current to percent reading (debug for now)
-
 }
