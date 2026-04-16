cc0600ed08a55d64c22baefcb6377ac34c171a07
Your Name <you@example.com>
2026-04-16 11:15:53 -0400

    test code basics are ready

--- pi_testbench/pi.py
+++ pi_testbench/pi.py
@@ -10,13 +10,14 @@ import serial
 class NanoACS(serial.Serial):
     @property
     def wiper(self) -> int:
-        self.write(b"?")
+        self.write(b":?\n")
         while True:
             line = self.readline().decode().strip()
             if not line:
                 raise TimeoutError("wiper: no response from gate Nano")
             try:
-                return int(line)
+                assert line.startswith(";P"), f"wiper: unexpected response: {line!r}"
+                return int(line[2:])
             except ValueError:
                 print(f"wiper skip: {line!r}")
 
@@ -52,17 +53,33 @@ def main():
             f"Version mismatch: got {response!r}, expected {version.strip()!r}"
         )
 
+        nanoACS.write(b"O")  # set ACCESS to open signal
+        nanoACS.readline()
+
         # loop
         while True:
             now = time.monotonic_ns()
             PERIOD = 10e9  # 10 seconds
-            angle_turns = now % PERIOD * 360 / PERIOD
-            value = (math.sin(angle_turns * 2 * math.pi) + 1) / 2 * 100
+            angle_turns = (now % PERIOD) / PERIOD
+            # sin wave
+            # value = (math.sin(angle_turns * 2 * math.pi) + 1) / 2 * 100
+            # square wave
+            value = 100 if angle_turns < 0.5 else 0
+
             # send value to gate
-            nanoACS.write(f"{int(value)}\n".encode())
+            nanoACS.write(f":S{int(value)}\n".encode())
+
+            # assert that the response to the write matches the expected value
+            response = nanoACS.readline().decode().strip()
+            assert response == f";S{int(value)}", (
+                f"Write response mismatch: got {response!r}, expected ;S{int(value)}"
+            )
+
             # read value from gate
             pos = nanoACS.wiper
-            print(f"pos: {pos}")
+            print(
+                f"pos: {pos}\t set: {response} \t time: {now} \t phase: {angle_turns:.2f}"
+            )  # evenly spaced columns
             # expecting chaos because that write causes a different response and the response is delayed like 1600ms or so
 
     finally:
