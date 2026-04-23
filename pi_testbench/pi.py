e98daf504624f5f87ac63ee51ff47fb42d636e9e
Your Name <you@example.com>
2026-04-23 12:22:16 -0400

    empirical testing in the group shows that this config seems okay.

--- pi_testbench/pi.py
+++ pi_testbench/pi.py
@@ -95,7 +95,7 @@ def main():
             # sin wave
             # value = (math.sin(angle_turns * 2 * math.pi) + 1) / 2 * 100
             # square wave
-            square_max, square_min = (25, 75)
+            square_max, square_min = (5, 70)
             value = square_max if angle_turns < 0.5 else square_min
 
             # send value to gate
--- test-nano/test-nano.ino
+++ test-nano/test-nano.ino
@@ -110,7 +110,7 @@ PositionSensor WIPER;
 //=============================================================================
 struct GateController {
     uint16_t wiperMin = 0;  // fallback; replace with EEPROM cal later
-    uint16_t wiperMax = 1023;  
+    uint16_t wiperMax = 1023;   
     uint16_t setpointPercent = 0; // uninitialized setpoint, is valid if you assume ACCESS is low during startup
     // in normal operation, setpoint will be bimodal, 0 and somewhere around 30, the second value is set over serial.
     int      deadband = 25;
